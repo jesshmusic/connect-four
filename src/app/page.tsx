3f01f0b142a2fec486d2e4f48549473da3e84e9e
jesshmusic <jesshmusic72@gmail.com>
2025-07-30 12:10:34 -0400

    feat: Creates components for the Player Tokens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import PlayerToken from '@/components/PlayerToken';
 // import { deepClone, checkForWinner } from "@/utils/connectFour";
 
 export default function Home() {
@@ -7,45 +8,10 @@ export default function Home() {
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>Connect Four</h1>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+        <div>
+          <h2>Sample Tokens</h2>
+          <PlayerToken player="red"/>
+          <PlayerToken player="yellow"/>
         </div>
       </main>
       <footer className={styles.footer}>
---
+++ src/components/PlayerToken.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+type PlayerColor = 'red' | 'yellow';
+
+interface PlayerTokenProps {
+  player: PlayerColor;
+}
+
+/**
+ * Token created using Afinity Design App. (Similar to Adobe Illustrator)
+ * @param player
+ * @constructor
+ */
+const PlayerToken = ({player}: PlayerTokenProps) => {
+  const playerColor = player === 'yellow' ? '#bc8e0d' : '#cc0000';
+  const playerColor2 = player === 'yellow' ? '#5d4505' : '#640000';
+  return (
+    <svg
+      width="100px"
+      height="100px"
+      viewBox="0 0 256 256">
+    <circle
+      cx="129.5"
+      cy="127.5"
+      r="121.5"
+      fill={playerColor}
+      stroke="black"
+      strokeWidth="2px"/>
+    <g transform="matrix(0.797929,0,0,0.797929,26.1683,26.2641)">
+      <circle
+        cx="129.5"
+        cy="127.5"
+        r="121.5"
+        fill={playerColor2}
+        stroke="black"
+        strokeWidth="2px"/>
+    </g>
+</svg>
+  )
+}
+
+export default PlayerToken;
